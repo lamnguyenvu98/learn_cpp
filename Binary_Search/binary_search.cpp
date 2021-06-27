63e0d85b5d28e9ba6b8f3960168685d6ad8dd9f4
lamnguyenvu98 <lamnguyenvu98@gmail.com>
2021-06-28 03:39:48 +0700

    updated

--- Binary_Search/binary_search.cpp
+++ Binary_Search/binary_search.cpp
@@ -19,9 +19,21 @@ int BinarySearch(vector<int> vec, int value)
     return -1;
 }
 
+int BinarySearch_Recursive(vector<int> vec, int value, int low, int high)
+{
+	int mid = low + (high - low)/2;
+	if (vec[mid] == value)
+		return mid;
+	else
+	{
+		if (value < vec[mid]) return BinarySearch_Recursive(vec, value, low,  mid - 1);
+		else return BinarySearch_Recursive(vec, value, mid  + 1, high);
+	}
+}
+
 int main()
 {
     vector<int> v {1, 4, 5, 7 ,8, 9, 12};
     sort(v.begin(), v.end());
-	cout << BinarySearch(v, 8) << endl;
+	cout << BinarySearch_Recursive(v, 8, 0, v.size() - 1) << endl;
 }
